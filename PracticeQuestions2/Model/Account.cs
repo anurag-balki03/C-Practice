d37abfbd09ede3b79b96f5ff71c4912575e37f67
anurag-balki03 <balkianurag04@gmail.com>
2025-03-05 11:31:19 +0530

    Add files via upload
    uploading Accounts and Upcasting and Downcasting

--- PracticeQuestions2/Model/Account.cs
+++ PracticeQuestions2/Model/Account.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PracticeQuestions2.Model
 {
@@ -16,27 +17,28 @@ namespace PracticeQuestions2.Model
 
     sealed class SavingsAccount : Account
     {
-        public int Principle { get; set; }
+        public int Principal { get; set; }
         public int Roi { get; set; }
         public int Time { get; set; }
         public Double Result { get; set; }
         public override void CalculateInterest(int flag)
         {
             Console.Write("Enter the Principle Amount : ");
-            Principle = int.Parse(Console.ReadLine());
+            Principal = int.Parse(Console.ReadLine());
             Console.Write("Enter the Rate Of Interest : ");
             Roi = int.Parse(Console.ReadLine());
             Console.Write("Enter the Time Period : ");
             Time = int.Parse(Console.ReadLine());
             if (flag == 1)
             {
-                Result = (Principle * Time * Roi) / 100;
+                Result = Principal+((Principal * Time * Roi) / 100);
             }
             else
             {
-                Result = Principle * Math.Pow((1 + Roi ), Time);
+                Result = Principal * Math.Pow((1 + (double)Roi / 100), Time);
+
             }
-            Console.WriteLine();
+            Console.WriteLine("Amouunt After Calculating Interest : " + Result);
         }
     }
 }
